274e3f7e4a2b8705daaca6467169048fbee4e9ef
AlexRPADev <66978272+AlexRPADev@users.noreply.github.com>
2020-10-14 20:47:16 -0700

    Add files via upload

---
+++ ASAPApp.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace ASAPApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.title = new System.Windows.Forms.Label();
+            this.questionOne = new System.Windows.Forms.Label();
+            this.questionTwo = new System.Windows.Forms.Label();
+            this.questionThree = new System.Windows.Forms.Label();
+            this.questionFour = new System.Windows.Forms.Label();
+            this.questionFive = new System.Windows.Forms.Label();
+            this.questionSix = new System.Windows.Forms.Label();
+            this.q1 = new System.Windows.Forms.TextBox();
+            this.q3 = new System.Windows.Forms.TextBox();
+            this.q4 = new System.Windows.Forms.TextBox();
+            this.q5 = new System.Windows.Forms.TextBox();
+            this.bttnCalculate = new System.Windows.Forms.Button();
+            this.displayWRPRIndex = new System.Windows.Forms.Label();
+            this.bttnReset = new System.Windows.Forms.Button();
+            this.q6 = new System.Windows.Forms.TextBox();
+            this.q2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Garamond", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(51, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(717, 30);
+            this.title.TabIndex = 0;
+            this.title.Text = "Daily \"Worth Index Regarding Plastic Recycling\" Calculator";
+            // 
+            // questionOne
+            // 
+            this.questionOne.AutoSize = true;
+            this.questionOne.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionOne.Location = new System.Drawing.Point(53, 59);
+            this.questionOne.Name = "questionOne";
+            this.questionOne.Size = new System.Drawing.Size(303, 18);
+            this.questionOne.TabIndex = 1;
+            this.questionOne.Text = "#1 How many plastic bags did I bought today?";
+            this.questionOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // questionTwo
+            // 
+            this.questionTwo.AutoSize = true;
+            this.questionTwo.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionTwo.Location = new System.Drawing.Point(53, 106);
+            this.questionTwo.Name = "questionTwo";
+            this.questionTwo.Size = new System.Drawing.Size(555, 18);
+            this.questionTwo.TabIndex = 2;
+            this.questionTwo.Text = "#2 When I was at shopping did I bring a bag with me from home? (n for No, y for Y" +
+    "es)";
+            this.questionTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // questionThree
+            // 
+            this.questionThree.AutoSize = true;
+            this.questionThree.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionThree.Location = new System.Drawing.Point(53, 152);
+            this.questionThree.Name = "questionThree";
+            this.questionThree.Size = new System.Drawing.Size(370, 18);
+            this.questionThree.TabIndex = 3;
+            this.questionThree.Text = "#3 How many plastic items did I throw at garbage today?";
+            this.questionThree.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // questionFour
+            // 
+            this.questionFour.AutoSize = true;
+            this.questionFour.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionFour.Location = new System.Drawing.Point(53, 198);
+            this.questionFour.Name = "questionFour";
+            this.questionFour.Size = new System.Drawing.Size(317, 18);
+            this.questionFour.TabIndex = 4;
+            this.questionFour.Text = "#4 How many plastic items did I recycled today?";
+            this.questionFour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // questionFive
+            // 
+            this.questionFive.AutoSize = true;
+            this.questionFive.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionFive.Location = new System.Drawing.Point(53, 248);
+            this.questionFive.Name = "questionFive";
+            this.questionFive.Size = new System.Drawing.Size(448, 18);
+            this.questionFive.TabIndex = 5;
+            this.questionFive.Text = "#5 How many plastic items I reused today instead of using new ones?";
+            this.questionFive.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // questionSix
+            // 
+            this.questionSix.AutoSize = true;
+            this.questionSix.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionSix.Location = new System.Drawing.Point(53, 298);
+            this.questionSix.Name = "questionSix";
+            this.questionSix.Size = new System.Drawing.Size(446, 18);
+            this.questionSix.TabIndex = 6;
+            this.questionSix.Text = "#6 Did my facts altered other people behaviors? (n for No, y for Yes)";
+            this.questionSix.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // q1
+            // 
+            this.q1.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1.Location = new System.Drawing.Point(638, 59);
+            this.q1.Name = "q1";
+            this.q1.Size = new System.Drawing.Size(100, 25);
+            this.q1.TabIndex = 7;
+            this.q1.TextChanged += new System.EventHandler(this.q1_TextChanged);
+            // 
+            // q3
+            // 
+            this.q3.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q3.Location = new System.Drawing.Point(638, 152);
+            this.q3.Name = "q3";
+            this.q3.Size = new System.Drawing.Size(100, 25);
+            this.q3.TabIndex = 9;
+            this.q3.TextChanged += new System.EventHandler(this.q3_TextChanged);
+            // 
+            // q4
+            // 
+            this.q4.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q4.Location = new System.Drawing.Point(638, 198);
+            this.q4.Name = "q4";
+            this.q4.Size = new System.Drawing.Size(100, 25);
+            this.q4.TabIndex = 10;
+            this.q4.TextChanged += new System.EventHandler(this.q4_TextChanged);
+            // 
+            // q5
+            // 
+            this.q5.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q5.Location = new System.Drawing.Point(638, 248);
+            this.q5.Name = "q5";
+            this.q5.Size = new System.Drawing.Size(100, 25);
+            this.q5.TabIndex = 11;
+            this.q5.TextChanged += new System.EventHandler(this.q5_TextChanged);
+            // 
+            // bttnCalculate
+            // 
+            this.bttnCalculate.Font = new System.Drawing.Font("Garamond", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnCalculate.Location = new System.Drawing.Point(56, 342);
+            this.bttnCalculate.Name = "bttnCalculate";
+            this.bttnCalculate.Size = new System.Drawing.Size(440, 96);
+            this.bttnCalculate.TabIndex = 13;
+            this.bttnCalculate.Text = "Calculate the \"worth index regarding plastic recycling\" NOW!";
+            this.bttnCalculate.UseVisualStyleBackColor = true;
+            this.bttnCalculate.Click += new System.EventHandler(this.bttnCalculate_Click);
+            // 
+            // displayWRPRIndex
+            // 
+            this.displayWRPRIndex.AutoSize = true;
+            this.displayWRPRIndex.Font = new System.Drawing.Font("Garamond", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayWRPRIndex.Location = new System.Drawing.Point(555, 342);
+            this.displayWRPRIndex.Name = "displayWRPRIndex";
+            this.displayWRPRIndex.Size = new System.Drawing.Size(0, 30);
+            this.displayWRPRIndex.TabIndex = 14;
+            this.displayWRPRIndex.Click += new System.EventHandler(this.displayWRPRIndex_Click);
+            // 
+            // bttnReset
+            // 
+            this.bttnReset.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnReset.Location = new System.Drawing.Point(560, 376);
+            this.bttnReset.Name = "bttnReset";
+            this.bttnReset.Size = new System.Drawing.Size(178, 62);
+            this.bttnReset.TabIndex = 15;
+            this.bttnReset.Text = "Reset entered values and recalibrate calculator!";
+            this.bttnReset.UseVisualStyleBackColor = true;
+            this.bttnReset.Click += new System.EventHandler(this.bttnReset_Click);
+            // 
+            // q6
+            // 
+            this.q6.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q6.Location = new System.Drawing.Point(638, 298);
+            this.q6.Name = "q6";
+            this.q6.Size = new System.Drawing.Size(100, 25);
+            this.q6.TabIndex = 12;
+            this.q6.TextChanged += new System.EventHandler(this.q6_TextChanged);
+            // 
+            // q2
+            // 
+            this.q2.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q2.Location = new System.Drawing.Point(638, 106);
+            this.q2.Name = "q2";
+            this.q2.Size = new System.Drawing.Size(100, 25);
+            this.q2.TabIndex = 8;
+            this.q2.TextChanged += new System.EventHandler(this.q2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttnReset);
+            this.Controls.Add(this.displayWRPRIndex);
+            this.Controls.Add(this.bttnCalculate);
+            this.Controls.Add(this.q6);
+            this.Controls.Add(this.q5);
+            this.Controls.Add(this.q4);
+            this.Controls.Add(this.q3);
+            this.Controls.Add(this.q2);
+            this.Controls.Add(this.q1);
+            this.Controls.Add(this.questionSix);
+            this.Controls.Add(this.questionFive);
+            this.Controls.Add(this.questionFour);
+            this.Controls.Add(this.questionThree);
+            this.Controls.Add(this.questionTwo);
+            this.Controls.Add(this.questionOne);
+            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ASAP APP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label questionOne;
+        private System.Windows.Forms.Label questionTwo;
+        private System.Windows.Forms.Label questionThree;
+        private System.Windows.Forms.Label questionFour;
+        private System.Windows.Forms.Label questionFive;
+        private System.Windows.Forms.Label questionSix;
+        private System.Windows.Forms.TextBox q1;
+        private System.Windows.Forms.TextBox q3;
+        private System.Windows.Forms.TextBox q4;
+        private System.Windows.Forms.TextBox q5;
+        private System.Windows.Forms.Button bttnCalculate;
+        private System.Windows.Forms.Label displayWRPRIndex;
+        private System.Windows.Forms.Button bttnReset;
+        private System.Windows.Forms.TextBox q6;
+        private System.Windows.Forms.TextBox q2;
+    }
+}
+
---
+++ ASAPApp.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ASAPApp
+{
+    public partial class Form1 : Form
+    {
+        double i1 = 0;
+        double i2y = 0;
+        double i2n = 0;
+        double i3 = 0;
+        double i4 = 0;
+        double i5 = 0;
+        double i6y = 0;
+        double i6n = 0;
+        int errors = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void q1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void q2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void q3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void q4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void q5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void q6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void bttnCalculate_Click(object sender, EventArgs e)
+        {
+            
+                if (!string.IsNullOrWhiteSpace(q1.Text))
+                {
+                    if (double.TryParse(q1.Text, out var value))
+                    {
+                        int que1 = int.Parse(q1.Text);
+                        double indexq1 = (que1 + 0.5) * que1;
+                        i1 = indexq1;
+                        //displayWRPRIndex.Text = indexq1.ToString("n2");
+                    }
+                    else
+                    {
+                        displayWRPRIndex.Text = "";
+                        MessageBox.Show($"Your answer for question #1 is '{q1.Text}'. This is not a number. Please enter a number!");
+                    errors = errors+1;
+                    }
+                }
+                else
+                {
+                    displayWRPRIndex.Text = "";
+                    MessageBox.Show("Please answer at question #1 with a number!");
+                errors = errors+1;
+            }
+
+
+
+
+                if (!string.IsNullOrWhiteSpace(q3.Text))
+                {
+                    if (double.TryParse(q3.Text, out var value))
+                    {
+                        int que3 = int.Parse(q3.Text);
+                        double indexq3 = (que3 + 0.9) * que3;
+                        i3 = indexq3;
+                        //displayWRPRIndex.Text = indexq3.ToString("n2");
+                    }
+                    else
+                    {
+                        displayWRPRIndex.Text = "";
+                        MessageBox.Show($"Your answer for question #3 is '{q3.Text}'. This is not a number. Please enter a number!");
+                    errors = errors+1;
+                }
+                }
+                else
+                {
+                    displayWRPRIndex.Text = "";
+                    MessageBox.Show("Please answer at question #3 with a number!");
+                errors = errors+1;
+            }
+
+                if (!string.IsNullOrWhiteSpace(q4.Text))
+                {
+                    if (double.TryParse(q4.Text, out var value))
+                    {
+                        int que4 = int.Parse(q4.Text);
+                        double indexq4 = (que4 + 0.7) * que4;
+                        i4 = indexq4;
+                        //displayWRPRIndex.Text = indexq4.ToString("n2");
+                    }
+                    else
+                    {
+                        displayWRPRIndex.Text = "";
+                        MessageBox.Show($"Your answer for question #4 is '{q4.Text}'. This is not a number. Please enter a number!");
+                    errors = errors+1;
+                }
+                }
+                else
+                {
+                    displayWRPRIndex.Text = "";
+                    MessageBox.Show("Please answer at question #4 with a number!");
+                errors = errors+1;
+            }
+
+
+                if (!string.IsNullOrWhiteSpace(q5.Text))
+                {
+                    if (double.TryParse(q5.Text, out var value))
+                    {
+                        int que5 = int.Parse(q5.Text);
+                        double indexq5 = -(que5 + 0.1) * que5;
+                        i5 = indexq5;
+                        //displayWRPRIndex.Text = indexq5.ToString("n2");
+                    }
+                    else
+                    {
+                        displayWRPRIndex.Text = "";
+                        MessageBox.Show($"Your answer for question #5 is '{q5.Text}'. This is not a number. Please enter a number!");
+                    errors = errors+1;
+                }
+                }
+                else
+                {
+                    displayWRPRIndex.Text = "";
+                    MessageBox.Show("Please answer at question #5 with a number!");
+                errors = errors+1;
+            }
+
+                if (!string.IsNullOrWhiteSpace(q2.Text))
+                {
+
+                    string que2str = q2.Text;
+
+                    if (que2str == "y")
+                    {
+                        double indexq2y = -2;
+                        i2y = indexq2y;
+                        //displayWRPRIndex.Text = indexq2y.ToString("n2");
+                    }
+                    else if (que2str == "n")
+                    {
+                        double indexq2n = 3;
+                        i2n = indexq2n;
+                        //displayWRPRIndex.Text = indexq2n.ToString("n2");
+                    }
+
+
+                    else
+                    {
+                        displayWRPRIndex.Text = "";
+                        MessageBox.Show($"Your answer for question #2 is '{q2.Text}'. This is not y or n. Please enter y for a 'YES' and n for a 'NO'!");
+                    errors = errors+1;
+                }
+                }
+                else
+                {
+                    displayWRPRIndex.Text = "";
+                    MessageBox.Show("Please answer at question #2 with y for a 'YES' and n for a 'NO'!");
+                errors = errors+1;
+            }
+
+
+
+                if (!string.IsNullOrWhiteSpace(q6.Text))
+                {
+
+                    string que6str = q6.Text;
+
+                    if (que6str == "y")
+                    {
+                        double indexq6y = -3;
+                        i6y = indexq6y;
+                        //displayWRPRIndex.Text = indexq6y.ToString("n2");
+                    }
+                    else if (que6str == "n")
+                    {
+                        double indexq6n = 2;
+                        i6n = indexq6n;
+                        //displayWRPRIndex.Text = indexq6n.ToString("n2");
+                    }
+
+
+                    else
+                    {
+                        displayWRPRIndex.Text = "";
+                        MessageBox.Show($"Your answer for question #6 is '{q6.Text}'. This is not y or n. Please enter y for a 'YES' and n for a 'NO'!");
+                    errors = errors+1;
+                }
+                }
+                else
+                {
+                    displayWRPRIndex.Text = "";
+                    MessageBox.Show("Please answer at question #6 with y for a 'YES' and n for a 'NO'!");
+                errors = errors+1;
+                
+            }
+            if (errors == 0)
+            {
+                double WRPRIndex = i1 + i2n + i2y + i3 + i4 + i5 + i6n + i6y;
+                //displayWRPRIndex.Text = errors.ToString();
+                displayWRPRIndex.Text = WRPRIndex.ToString("n2");
+
+            } else
+            {
+                //displayWRPRIndex.Text = errors.ToString();
+                errors = 0;
+
+            }
+
+                //Application.Restart(500ms);
+                //bttnCalculate.Enabled = false;
+
+
+        }
+
+        private void displayWRPRIndex_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnYesTwo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnNoTwo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnReset_Click(object sender, EventArgs e)
+        {
+            displayWRPRIndex.Text = "";
+            q1.Text = "";
+            q2.Text = "";
+            q3.Text = "";
+            q4.Text = "";
+            q5.Text = "";
+            q6.Text = "";
+        }
+
+
+
+    }
+}
+
